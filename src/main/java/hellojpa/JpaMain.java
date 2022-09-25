d00b5918622adcb35c237cf347fa205f4b886276
HaeChan Kim <bluesun147@naver.com>
2022-09-25 19:24:25 +0900

    플러시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -22,38 +22,15 @@ public class JpaMain {
         tx.begin(); // 트랜잭션 시작
 
         try {
-
-            /*
-            // 여기까지는 비영속. 아무상태도 아님.
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-
-            // 여기서부터 영속상태
-            // em 내부에 영속성 컨텍스트 통해서 이 멤버가 관리됨.
+            System.out.println("~~~~~~~~~~~~");
+            // 영속
+            Member member = new Member(200L, "member200");
             em.persist(member);
-
-            // db에서 가져온 것이 아니라 1차 캐시에서 조회.
-            // 실행해 보면 select문 없음
-            Member findMember = em.find(Member.class, 101L);
-            Member findMember2 = em.find(Member.class, 101L);
-            System.out.println("findMember = " + findMember.getName());
-            System.out.println(findMember == findMember2); // 영속 엔티티의 동일성 보장
-            */
-
-//            Member member1 = new Member(150L, "A");
-//            Member member2 = new Member(160L, "B"); // db에 저장했으니 우선 주석처리
-            Member member = em.find(Member.class, 150L);
-            member.setName("zzzz"); // 변경 후
-            // em.persist(member); // 할 필요 없음
-
-//            em.persist(member1);
-//            em.persist(member2);
-            System.out.println("===========");
-
-            tx.commit(); // 정상이면 커밋하고
-            // persist 까지는 insert 쿼리 db에 보내지 않음. commit 하는 순간에 보냄
-            // 그동안 쓰기 지연 sql 저장소에 저장해둠.
+            System.out.println("=====1");
+            em.flush(); // 강제 호출
+            System.out.println("=====2");
+            System.out.println("=====3");
+            tx.commit();
 
         } catch (Exception e) {
             tx.rollback(); // 문제 생기면 롤백
